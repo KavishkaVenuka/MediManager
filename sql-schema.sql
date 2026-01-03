3ac55884085d83df3a93cd741d8ee586f2ad37c5
kavishka-venuka <kavishkavenuka554pro@gmail.com>
2026-01-03 22:25:50 +0530

    Change the SQL schema accordin to Normalization concept

--- sql-schema.sql
+++ sql-schema.sql
@@ -1,7 +1,5 @@
--- 1. Enable UUIDs
-CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
--- 2. Medicine Table (The Catalog)
+-- 1. Medicine Table (The Catalog)
 CREATE TABLE medicine (
     id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     name VARCHAR(255) NOT NULL,
@@ -9,35 +7,35 @@ CREATE TABLE medicine (
     pack_size INTEGER NOT NULL DEFAULT 1
 );
 
--- 3. Main Store (Bulk Storage)
+-- 2. Main Store (Bulk Storage)
 CREATE TABLE main_store (
     item_id UUID REFERENCES medicine(id) ON DELETE CASCADE,
-    buy_price DECIMAL(10, 2) NOT NULL,
+    retail_price DECIMAL(10, 2) NOT NULL,
     pack_qty INTEGER DEFAULT 0,
     pill_qty INTEGER DEFAULT 0,
     last_updated TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
-    PRIMARY KEY (item_id, buy_price)
+    PRIMARY KEY (item_id, retail_price)
 );
 
--- 4. Pharmacy Table (The Selling Shelf)
+-- 3. Pharmacy Table (The Selling Shelf)
 -- Keeps the Composite Key (item_id + buy_price) to track batches/profit
 CREATE TABLE pharmacy (
     item_id UUID REFERENCES medicine(id) ON DELETE CASCADE,
-    buy_price DECIMAL(10, 2) NOT NULL,
+    retail_price DECIMAL(10, 2) NOT NULL,
     pack_qty INTEGER DEFAULT 0,
     pill_qty INTEGER DEFAULT 0,
     last_updated TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
-    PRIMARY KEY (item_id, buy_price)
+    PRIMARY KEY (item_id, retail_price)
 );
 
--- 4.5 Point of Sale (Ready to Sell Stock)
+-- 4. Point of Sale (Ready to Sell Stock)
 CREATE TABLE point_of_sale (
     item_id UUID REFERENCES medicine(id) ON DELETE CASCADE,
-    buy_price DECIMAL(10, 2) NOT NULL,
+    retail_price DECIMAL(10, 2) NOT NULL,
     pack_qty INTEGER DEFAULT 0,
     pill_qty INTEGER DEFAULT 0,
     last_updated TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
-    PRIMARY KEY (item_id, buy_price)
+    PRIMARY KEY (item_id, retail_price)
 );
 
 -- 5. Inward Register (Stock Entry Log)
@@ -51,6 +49,23 @@ CREATE TABLE inward_register (
     buy_price DECIMAL(10, 2) NOT NULL,
     retail_price DECIMAL(10, 2) NOT NULL,
     created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+);
+
+-- 6. Sales (Stock Exit Log)
+CREATE TABLE sales (
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    total_amount DECIMAL(10, 2) NOT NULL
+);
+
+-- 7. Sale Items (Stock Exit Log)
+CREATE TABLE sale_items (
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    sale_id UUID REFERENCES sales(id),
+    item_id UUID REFERENCES medicine(id),
+    qty INTEGER NOT NULL,
+    unit_buy_price DECIMAL(10, 2) NOT NULL, 
+    unit_sell_price DECIMAL(10, 2) NOT NULL
 );
 
 
