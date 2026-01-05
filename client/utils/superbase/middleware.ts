dccd0ead4075c9828f05630011044a4557aa5ced
kavishka-venuka <kavishkavenuka554pro@gmail.com>
2026-01-05 19:59:38 +0530

    Implement middleware for supabase

--- client/utils/superbase/middleware.ts
+++ client/utils/superbase/middleware.ts
@@ -45,7 +45,22 @@ export async function updateSession(request: NextRequest) {
     // If this is not done, you may be causing the browser and server to go out
     // of sync and terminate the user's session prematurely!
 
-    await supabase.auth.getUser()
+    const {
+        data: { user },
+    } = await supabase.auth.getUser()
+
+    if (!user && !request.nextUrl.pathname.startsWith('/login')) {
+        // no user, potentially respond by redirecting the user to the login page
+        const url = request.nextUrl.clone()
+        url.pathname = '/login'
+        return NextResponse.redirect(url)
+    }
+
+    if (user && request.nextUrl.pathname.startsWith('/login')) {
+        const url = request.nextUrl.clone()
+        url.pathname = '/'
+        return NextResponse.redirect(url)
+    }
 
     return supabaseResponse
 }
